f51eb47133caa0fa0bd8b78fffb33587c29f6fe7
stu@anvil.works <stu@anvil.works>
2024-06-07 13:30:55 +0800

    Edited routes

--- client_code/routes.py
+++ client_code/routes.py
@@ -40,5 +40,13 @@ class ArticlesRoute(Route):
 
     @staticmethod
     def loader():
+        # could use context here - we need to be paricularly careful about open form
+        # because open form can be asyncronous in some ways
+        # i.e. multiple open forms can be called in flight
+        # so if we use context - we'll want to do our clever __new__ thing
+        # attach the context (maybe through a weak map)
+        # then call the API with self
+        # e.g. routing.get_data(self) - probably
+        # caching will be fun here
+        # search params, path params
         return anvil.server.call("load_articles")
-
