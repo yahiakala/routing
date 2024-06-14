cd3fe2becf4b0a65c434014123e819d2214f4df2
stu <stu@anvil.works>
2024-06-14 12:23:57 +0800

    see if we can avoid the double import

--- client_code/router/routes.py
+++ client_code/router/routes.py
@@ -1,5 +1,7 @@
+import anvil.server
+
 from .segments import Segment
-from .utils import trim_path
+from .utils import trim_path, encode_search_params
 
 
 sorted_routes = []
@@ -30,3 +32,25 @@ class Route:
         cls.path = trim_path(cls.path)
         cls.segments = Segment.from_path(cls.path)
         sorted_routes.append(cls())
+        if anvil.is_server_side():
+            cls.create_server_route()
+
+
+    @classmethod
+    def create_server_route(cls):
+        # local for now while anvil uplink doesn't have history
+        from anvil.history import Location
+        from .loader import load_data, cache
+        from .matcher import get_match
+
+        path = cls.path
+
+        @anvil.server.route(path)
+        def route_handler(*args, **kwargs):
+            request = anvil.server.request
+            path = request.path
+            search = encode_search_params(request.query_params)
+            location = Location(path=path, search=search, key="default")
+            match = get_match(location=location)
+            load_data(match)
+            return anvil.server.LoadAppResponse(data={"cache": cache})
