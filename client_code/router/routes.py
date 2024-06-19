72742ce1603938f971bbf811b17f886471973492
stu <stu@anvil.works>
2024-06-19 14:59:11 +0800

    fix route handler

--- client_code/router/routes.py
+++ client_code/router/routes.py
@@ -45,7 +45,7 @@ class Route:
     def create_server_route(cls):
         # local for now while anvil uplink doesn't have history
         from anvil.history import Location
-        from .loader import load_data, cache
+        from .loader import load_data, cache, CachedData
         from .matcher import get_match
 
         path = cls.path
@@ -59,5 +59,22 @@ class Route:
             search = encode_search_params(request.query_params)
             location = Location(path=path, search=search, key="default")
             match = get_match(location=location)
-            load_data(match)
+            if match is None:
+                raise Exception("No match")
+
+            route = match.route
+            search_params = match.search_params
+            path_params = match.path_params
+            deps = match.deps
+
+            data = route.loader(
+                location=location,
+                search_params=search_params,
+                path_params=path_params,
+                deps=deps,
+            )
+
+            cached_data = CachedData(data=data, location=location)
+            cache = {match.key: cached_data}
+
             return anvil.server.LoadAppResponse(data={"cache": cache})
