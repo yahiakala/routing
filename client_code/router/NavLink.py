556de0df9c12e972a699ce72d054c530fd45ba9b
stu <stu@anvil.works>
2024-06-20 11:46:19 +0800

    more proof of concept

--- client_code/router/NavLink.py
+++ client_code/router/NavLink.py
@@ -3,22 +3,49 @@ from anvil.js import get_dom_node
 from ._navigate import navigate
 from anvil.history import history
 
-def default_link(href, **properties):
-    link_component = anvil.Link(source=href)
-    return link_component
+
+class DefaultLink(anvil.Link):
+    def __init__(self, **properties):
+        super().__init__(**properties)
+
+    @property
+    def href(self):
+        return self.source
+
+    @href.setter
+    def href(self, value):
+        self.source = value
 
 
 class NavLink(anvil.Component):
-    _anvil_properties_ = [{"name": "href", "type": "string"}]
+    _anvil_properties_ = [
+        {"name": "href", "type": "string"},
+        {"name": "text", "type": "string"},
+    ]
     _anvil_events_ = [{"name": "click", "defaultEvent": True}]
 
     def __init__(self, **properties):
         self.href = properties.get("href")
-        self._link = default_link(href=self.href)
+        self._link = DefaultLink(href=self.href)
         self._link.add_event_handler("x-anvil-page-added", self._setup)
         self._link.add_event_handler("x-anvil-page-removed", self._cleanup)
 
-    
+    @property
+    def href(self):
+        return self._link.href
+
+    @href.setter
+    def href(self, value):
+        self._link.href = value
+
+    @property
+    def text(self):
+        return self._link.text
+
+    @text.setter
+    def text(self, value):
+        self._link.text = value
+
     def _on_click(self, e):
         if e.ctrlKey or e.metaKey or e.shiftKey:
             return
@@ -27,23 +54,21 @@ class NavLink(anvil.Component):
 
         href = self.href
         navigate(path=href)
-    
+
     def _setup(self):
         self._el = get_dom_node(self._link)
         self._el.addEventListener("click", self._on_click, True)
-    
+
     def _cleanup(self):
         el = self._el
         if el is None:
             return
         self._el = None
         el.removeEventListener("click", self._on_click, True)
-        
+
     def _anvil_setup_dom_(self):
         return self._link._anvil_setup_dom_()
-    
+
     @property
     def _anvil_dom_element_(self):
         return self._link._anvil_dom_element_
-        
-
