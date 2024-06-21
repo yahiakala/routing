5f4ea363d707773cdb9c936a3d62fce4aa0d1b78
stu <stu@anvil.works>
2024-06-21 10:30:52 +0800

    export cache mode

--- client_code/router/__init__.py
+++ client_code/router/__init__.py
@@ -2,3 +2,4 @@ from ._router import create, NavigationBlocker, UnloadBlocker
 from ._navigate import navigate
 from ._route import Route
 from ._redirect import Redirect
+from ._constants import NETWORK_FIRST, STALE_WHILE_REVALIDATE
