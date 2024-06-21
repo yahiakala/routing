0500f9c1bdf05b7a7f5cbe67e439dd0ff4cffee0
stu <stu@anvil.works>
2024-06-21 13:00:04 +0800

    fix timeout

--- client_code/router/_utils/client.py
+++ client_code/router/_utils/client.py
@@ -4,14 +4,16 @@ from anvil.js.window import Promise, setTimeout, URLSearchParams, document
 from anvil.js import await_promise
 from anvil.js import report_exceptions
 
-def timeout(ms=0):
+
+def timeout(s=0):
     def wait_async(resolve, reject):
         def timeout():
             resolve(TIMEOUT)
 
-        setTimeout(timeout, ms)
+        setTimeout(timeout, s * 1000)
 
     return Promise(wait_async)
+
 
 def encode_search_params(search_params):
     if not search_params:
