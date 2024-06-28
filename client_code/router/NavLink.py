affdf092eb76a2960cfdad335fe4f135aca8290d
stu <stu@anvil.works>
2024-06-28 14:52:59 +0800

    full URLs

--- client_code/router/NavLink.py
+++ client_code/router/NavLink.py
@@ -138,7 +138,7 @@ class NavLink(anvil.Container):
         if in_designer:
             self._form = _temp_hack_to_get_form(self)
         elif location.path is not None:
-            self._link.href = self._href = str(location)
+            self._link.href = self._href = location.get_url(False)
 
     @property
     def nav_args(self):
