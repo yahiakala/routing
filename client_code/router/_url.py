aa46e3a40e6318a92e7e94ff6bf0d42919a7f4ad
stu <stu@anvil.works>
2024-07-11 20:54:40 +0800

    ok testing this out

--- client_code/router/_url.py
+++ client_code/router/_url.py
@@ -1,6 +1,6 @@
 from ._navigate import nav_args_to_location
 from anvil.history import history
 
-def get_url(*, path, path_params, search_params, hash):
+def get_url(*, path, path_params=None, search_params=None, hash=None):
     location = nav_args_to_location(path=path, path_params=path_params, search_params=search_params, hash=hash) 
     history.createHref(location)
