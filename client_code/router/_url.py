2052baead8bd0eddeb0380785a78d1919bf807ce
stu <stu@anvil.works>
2024-07-11 20:58:09 +0800

    fix chaned api

--- client_code/router/_url.py
+++ client_code/router/_url.py
@@ -3,4 +3,4 @@ from anvil.history import history
 
 def get_url(*, path, path_params=None, search_params=None, hash=None):
     location = nav_args_to_location(path=path, path_params=path_params, search_params=search_params, hash=hash) 
-    history.createHref(location)
+    return location.get_url(False)
