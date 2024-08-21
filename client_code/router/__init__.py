08fd8e12a8731a76f301a9cf0e686d54e99a8ca3
stu <stu@anvil.works>
2024-08-21 13:39:29 +0800

    support some history methods

--- client_code/router/__init__.py
+++ client_code/router/__init__.py
@@ -1,4 +1,6 @@
 # ruff: noqa: F401
+from anvil.history import history as _history
+
 from ._cached import clear_cache
 from ._constants import NETWORK_FIRST, STALE_WHILE_REVALIDATE
 from ._exceptions import NotFound, Redirect
@@ -7,3 +9,19 @@ from ._navigate import navigate
 from ._route import Route, open_form
 from ._router import NavigationBlocker, UnloadBlocker, launch
 from ._url import get_url
+
+
+def go(n=0):
+    return _history.go(n)
+
+
+def back():
+    return _history.go(-1)
+
+
+def forward():
+    return _history.go(1)
+
+
+def reload():
+    return _history.reload()
